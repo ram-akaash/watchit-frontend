8cbb294c38d8d2f8d62f82da37cc1e92c1425f30
ram-akaash <103123321+ram-akaash@users.noreply.github.com>
2024-06-03 12:37:41 +0530

    first commit

---
+++ src/scenes/home/Home.jsx
@@ -0,0 +1,13 @@
+
+
+
+function Home() {
+  return (
+    <div className="home">
+    
+      
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/scenes/home/MainCarousel.jsx
@@ -0,0 +1,6 @@
+import { Box, Typography, IconButton, useMediaQuery } from "@mui/material";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import { shades } from "../../theme";
